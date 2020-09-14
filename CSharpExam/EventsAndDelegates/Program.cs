5b36cd638b73ee9bdf2af04001a380e2b036f214
Josh <joshharvey@btopenworld.com>
2020-09-14 16:06:28 +0100

    Custom Delegates

--- CSharpExam/EventsAndDelegates/Program.cs
+++ CSharpExam/EventsAndDelegates/Program.cs
@@ -1,36 +1,77 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
 
-namespace EventsAndDelegates {
-    internal class Alarm {
-        public event EventHandler OnAlarmRaised = delegate {  };
+namespace EventsAndDelegates
+{
+    internal class Alarm
+    {
+        public event EventHandler<AlarmEventArgs> OnAlarmRaised = delegate { };
 
-        public class AlarmEventArgs : EventArgs {
-            public string Location { get; set; }
-            public AlarmEventArgs(string location) {
+        public class AlarmEventArgs : EventArgs
+        {
+            public string Location { get; }
+
+            public AlarmEventArgs(string location)
+            {
                 Location = location;
             }
         }
 
-        public void RaiseAlarm(string location) {
+        public void RaiseAlarm(string location)
+        {
+            var exceptionList = new List<Exception>();
+            foreach (var @delegate in OnAlarmRaised.GetInvocationList())
+            {
+                try
+                {
+                    @delegate.DynamicInvoke(this, new AlarmEventArgs(location));
+                }
+                catch (TargetInvocationException e)
+                {
+                    exceptionList.Add(e.InnerException);
+                }
+            }
+
+            if (exceptionList.Count > 0)
+            {
+                throw new AggregateException(exceptionList);
+            }
+
             OnAlarmRaised(this, new AlarmEventArgs(location));
         }
     }
 
-    internal static class Program {
-        private static void Main(string[] args) {
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
             var alarm = new Alarm();
             alarm.OnAlarmRaised += AlarmListener1;
             alarm.OnAlarmRaised += AlarmListener2;
-            
-            alarm.RaiseAlarm("London");
+
+            try
+            {
+                alarm.RaiseAlarm("London");
+            }
+            catch (AggregateException agg)
+            {
+                foreach (var exception in agg.InnerExceptions)
+                {
+                    Console.WriteLine(exception.Message);
+                }
+            }
         }
 
-        private static void AlarmListener1(object sender, EventArgs e) {
-            Console.WriteLine("Alarm 1 called by " + sender);
+        private static void AlarmListener1(object sender, Alarm.AlarmEventArgs e)
+        {
+            Console.WriteLine("Alarm 1 called by " + e.Location);
+            throw new Exception("boom");
         }
 
-        private static void AlarmListener2(object sender, EventArgs e) {
-            Console.WriteLine("Alarm 2 called by " + sender);
+        private static void AlarmListener2(object sender, Alarm.AlarmEventArgs e)
+        {
+            Console.WriteLine("Alarm 2 called by " + e.Location);
         }
     }
 }
