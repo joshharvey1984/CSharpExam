b128ab9d15eab23eaee24267e4992fa1fd1b2a82
Josh <joshharvey@btopenworld.com>
2020-09-14 16:09:00 +0100

    Custom Delegates

---
+++ CSharpExam/CustomDelegate/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CustomDelegate
+{
+    static class Program
+    {
+        delegate int IntOperation(int a, int b);
+
+        private static int Add(int a, int b)
+        {
+            Console.WriteLine("Add called");
+            return a + b;
+        }
+
+        private static int Subtract(int a, int b)
+        {
+            Console.WriteLine("Subtract called");
+            return a - b;
+        }
+        
+        static void Main(string[] args)
+        {
+            var op = new IntOperation(Add);
+            Console.WriteLine(op(2, 2));
+
+            op = Subtract;
+            Console.WriteLine(op(3, 2));
+        }
+    }
+}
